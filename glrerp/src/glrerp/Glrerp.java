bd360738f71bf220d5f7cb4a5538c20eebc64c68
ruang <ruang@DESKTOP-B6V59E4>
2023-05-11 23:04:41 -0300

    Ultima atualização 11/05/22

--- glrerp/src/glrerp/Glrerp.java
+++ glrerp/src/glrerp/Glrerp.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package glrerp;
 
 import view.telaPrincipal;
 
 /**
  *
- * @author rg
+ * @author Gabriel Nierich, Luiz Gemmer e Ruan Gemmer
  */
 public class Glrerp {
 
@@ -16,10 +13,7 @@ public class Glrerp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        //agora vai
-        //poraaaa!!!
         // Pull from upstream
         
         new telaPrincipal().setVisible(true);
